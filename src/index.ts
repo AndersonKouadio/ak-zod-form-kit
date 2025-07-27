0bb431f65409c5f15d99049777de45928230bcc0
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-07-27 04:29:51 +0000

    mise à jour de la version 1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,8 @@
-// Point d'entrée principal de ZodFormKit
-export { processAndValidateFormData } from './processAndValidateFormData';
-export { extractDataFromFormData } from './extractDataFromFormData';
-export { applyDataTransformations } from './applyDataTransformations';
-export { convertObjectToFormData } from './convertObjectToFormData';
-export { createDynamicZodSchema } from './createDynamicZodSchema';
-export { validateDataWithDynamicSchema } from './validateDataWithDynamicSchema';
-export { 
-  formatZodErrorsAsObject, 
-  formatZodErrorsAsArray 
-} from './zod-error-formatters';
-
-// Export des types
-export type {
-  FormDataExtractionOptions,
-  FormDataProcessingOptions,
-  SchemaFieldType,
-  ProcessedFormDataResult
-} from './types';
+export * from './applyDataTransformations';
+export * from './convertObjectToFormData';
+export * from './createDynamicZodSchema';
+export * from './extractDataFromFormData';
+export * from './processAndValidateFormData';
+export * from './types';
+export * from './validateDataWithDynamicSchema';
+export * from './zod-error-formatters';
